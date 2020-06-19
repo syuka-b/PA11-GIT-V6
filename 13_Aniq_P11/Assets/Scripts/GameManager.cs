fe540ea6bd6a827e300409d76d2d978df0349a98
arupha <64124392+syuka-b@users.noreply.github.com>
2020-06-19 09:19:54 +0800

    Added unity project

---
+++ 13_Aniq_P11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager thisManager = null;
+    [SerializeField] private Text Txt_Score = null;
+    private int Score = 0;
+    private int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                Score += value;
+        Txt_Score.text = "SCORE : " + Score;
+    }
+    public void UpdateScore(int value)
+    {
+        Score += value;
+        Txt_Score.text = "SCORE : " + Score;
+    }
+    public void restarting()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ 13_Aniq_P11/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Avoid")
+        {
+            GameManager.thisManager.UpdateScore(1);
+        }
+    }
+    
+}
